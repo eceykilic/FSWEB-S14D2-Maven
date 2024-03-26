ddfcc7c247d5dc84b3de98121e6f43a08aaaff19
eceykilic <eceykilic@gmail.com>
2024-03-26 12:40:32 +0300

    solutions

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,6 @@
+import org.example.enums.LampType;
+import org.example.enums.PaintColor;
 import org.example.model.*;
-import org.example.model.enums.LampType;
-import org.example.model.enums.PaintColor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
